2ce8b0d5eafa0a9b144588baa295154c7e095455
JamesAlexander-rockwall <james.alexander.rockwall@gmail.com>
2021-01-23 17:04:09 +0200

    feat - implement hello world example

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>
+    Hello, world!
+  </h1>,
   document.getElementById('root')
 );
 
